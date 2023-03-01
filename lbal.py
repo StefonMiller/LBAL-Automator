8357b8ff9aad34df4d933478ea2b0613684915a4
stefonmiller <stefonplays@gmail.com>
2023-02-28 20:00:13 -0500

    Added basic spin automation

--- lbal.py
+++ lbal.py
@@ -1,12 +1,14 @@
 from PIL import Image
-from pytesseract import pytesseract
+# from pytesseract import pytesseract
 import pyautogui
 from pywinauto import Application
 from pywinauto import findwindows
+from pywinauto import mouse
 import win32gui
 import time
 import os
 import cv2
+import math
 import numpy as np
 
 def mse(img1, img2):
@@ -40,6 +42,65 @@ def get_text_from_image(path):
     # Displaying the extracted text
     return text[:-1]
 
+def click_play_button():
+    return
+
+def click_floor_button():
+    return
+
+def click_start_button():
+    return
+
+def click_spin_button():
+    time.sleep(.5)
+    mouse.click(button='left', coords=(int(spin_button_x), int(spin_button_y)))
+    time.sleep(.5)
+
+def dismiss_email():
+    time.sleep(.5)
+    mouse.click(button='left', coords=(int(email_button_x), int(email_button_y)))
+    time.sleep(.5)
+
+def click_skip_button():
+    mouse.click(button='left', coords=(int(skip_button_x), int(skip_button_y)))
+
+
+# Determine where we currently are
+def current_screen(i):
+    # Check if at title
+    p = pyautogui.screenshot()
+    p.save(rf'temp{i}.png')
+    if compare_images(rf'temp{i}.png', r'cur\home.png'):
+        print("At home screen")
+        return
+    elif compare_images(rf'temp{i}.png', r'cur\play.png'):
+        print('At play screen')
+        return
+    p = pyautogui.screenshot(region=(spin_start_x, spin_start_y, (spin_end_x - spin_start_x), (spin_end_y - spin_start_y)))
+    p.save(rf'temp{i}.png')
+    if compare_images(rf'temp{i}.png', r'cur\spin.png'):
+        print('At spin screen')
+        click_spin_button()
+        return
+    
+    # Check header
+    p = pyautogui.screenshot(region=(header_start_x, header_start_y, (header_end_x - header_start_x), (header_end_y - header_start_y)))
+    p.show
+    p.save(rf'temp{i}.png')
+    if compare_images(rf'temp{i}.png', r'cur\email.png'):
+        print('At email screen')
+        dismiss_email()
+        return
+    elif compare_images(rf'temp{i}.png', r'cur\symbol.png'):
+        print('At symbol addition screen')
+        click_skip_button()
+        return
+    elif compare_images(rf'temp{i}.png', r'cur\item.png'):
+        print('At item addition screen')
+        return
+
+    print('I have no idea where I\'m at, please help')
+    return
 
 # Defining paths to tesseract.exe
 # and the image we would be using
@@ -54,7 +115,7 @@ print(f'Current screen width: {curr_screen_width}\nCurrent screen height: {curr_
 
 
 # Can be used for both email and symbol/item addition
-header_start_x = (119 / 1920) * curr_screen_width
+header_start_x = math.floor((119 / 1920) * curr_screen_width)
 header_start_y = (137 / 1080) * curr_screen_height
 header_end_x = (1796 / 1920) * curr_screen_width
 header_end_y = (223 / 1080) * curr_screen_height
@@ -88,7 +149,7 @@ spin_button_x = (962 / 1920) * curr_screen_width
 spin_button_y = (1015 / 1080) * curr_screen_height
 
 skip_button_x = (965 / 1920) * curr_screen_width
-spin_button_y = (284 / 1080) * curr_screen_height
+skip_button_y = (284 / 1080) * curr_screen_height
 
 retry_button_x = (955 / 1920) * curr_screen_width
 retry_button_y = (909 / 1080) * curr_screen_height
@@ -99,6 +160,7 @@ email_button_y = (968 / 1080) * curr_screen_height
 pay_button_x = (950 / 1920) * curr_screen_width
 pay_button_y = (985 / 1080) * curr_screen_height
 
+# TODO: Get locations for rerolls + items when reroll is present. Get location of floor 1 select screen
 
 hwnd = win32gui.FindWindow(None, 'Luck Be a Landlord')
 
@@ -106,11 +168,10 @@ win32gui.SetForegroundWindow(hwnd)
 win32gui.ShowWindow(hwnd, 9)
 
 time.sleep(.5)
-# 
-p = pyautogui.screenshot(region=(header_start_x, header_start_y, (header_end_x - header_start_x), (header_end_y - header_start_y)))
-p.save(r'temp.png')
-
-print(pyautogui.position())
-
-
+i = 0
+while(1):
+    current_screen(i)
+    time.sleep(3)
+    i = i +1
+win32gui.ShowWindow(hwnd, 6)
 
